eea30d10791f01dee125334068d6277b228d8e68
Asma Ben Boubaker <36500896+asmabenboubaker@users.noreply.github.com>
2024-06-03 14:46:16 +0200

    gantt devextreme

--- src/app/pages/Oportunite/projet/static-project/static-project.component.ts
+++ src/app/pages/Oportunite/projet/static-project/static-project.component.ts
@@ -1,25 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import { TaskFieldsModel } from '@syncfusion/ej2-angular-gantt';
-import {ProjectService} from "../../../../Service/project.service";
-import {ActivatedRoute, Router} from "@angular/router";
-import {TaskServiceService} from "../../../../Service/task-service.service";
+import { HttpClient } from "@angular/common/http";
+import { ProjectService } from "../../../../Service/project.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { TaskServiceService } from "../../../../Service/task-service.service";
 import { registerLicense } from '@syncfusion/ej2-base';
+
 registerLicense('Ngo9BigBOggjHTQxAR8/V1NBaF1cXmhPYVFxWmFZfVpgfV9GZlZVTGYuP1ZhSXxXdkBhUX9XcHJRT2leUEM=');
+
 @Component({
   selector: 'app-static-project',
   templateUrl: './static-project.component.html',
   styleUrls: ['./static-project.component.scss']
 })
 export class StaticProjectComponent implements OnInit {
-  // start pie chart
+  areas: any[] = [];
+  idprojet: any;
+  tasks: any[] = [];
+    data: any;
 
-  areas: any[];
-idprojet:any;
+  constructor(
+      private http: HttpClient,
+      private projetService: ProjectService,
+      private router: Router,
+      private taskService: TaskServiceService,
+      public route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.idprojet = this.route.snapshot.paramMap.get('id');
+    this.fetchDemandsByDomain();
+    this.getProjets();
+  }
+
   fetchDemandsByDomain() {
     this.http.get<any>('http://localhost:8888/demo_war/api/tasks/1/task-status-statistics')
         .subscribe(data => {
-          // Assuming data is in the format [{ status: 'a faire', count: 1 }, { status: 'en cours', count: 2 }, ... ]
           this.areas = data.map(entry => ({ country: entry.status, area: entry.count }));
           console.log("areas", this.areas);
         });
@@ -28,10 +43,10 @@ idprojet:any;
   pointClickHandler(e) {
     this.toggleVisibility(e.target);
   }
+
   legendClickHandler(e) {
     const arg = e.target;
     const item = e.component.getAllSeries()[0].getPointsByArg(arg)[0];
-
     this.toggleVisibility(item);
   }
 
@@ -42,30 +57,21 @@ idprojet:any;
       item.show();
     }
   }
-  // end pie chart
-
-
-  //start gantt
-  public data: any[] = [];
-  public taskSettings: TaskFieldsModel | undefined;
-
-  constructor(private http: HttpClient,private projetService : ProjectService,private router: Router,private taskService :TaskServiceService,
-              public route: ActivatedRoute
-  ) { }
-
-  ngOnInit(): void {
-    this.idprojet=this.route.snapshot.paramMap.get('id');
-    this.fetchDemandsByDomain();
-    // gantt
-    this.taskSettings = {id: 'id', name: 'subject', startDate: 'start_date', endDate: 'end_date', duration: 'Duration', progress: 'Progress', child: 'subtasks' };
-    //set data methode form service
-
-this.getProjets();
-  }
-
 
   getProjets(): void {
+    this.taskService.getTasks(this.idprojet).subscribe(projets => {
+      this.data = projets;
+      this.transformData();
+    });
+  }
 
-    this.taskService.getTasks(this.idprojet).subscribe(projets => this.data = projets);}
-
+  transformData() {
+    this.tasks = this.data.map(item => ({
+      id: item.id,
+      title: item.subject,
+      start: new Date(item.start_date),
+      end: new Date(item.end_date),
+    }));
+    console.log('Transformed tasks:', this.tasks);
+  }
 }
