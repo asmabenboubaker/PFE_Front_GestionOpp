eb1d9c55ce1c2ccd725cc02630485f38752452ef
Asma Ben Boubaker <36500896+asmabenboubaker@users.noreply.github.com>
2024-06-07 21:24:59 +0200

    display files

---
+++ src/app/Service/file-service.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {WsService} from "../../ws.service";
+import {EnvService} from "../../env.service";
+import {TokenStorageService} from "../pages/Global/shared-service/token-storage.service";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileServiceService {
+
+  constructor(private http: HttpClient,private Wservice: WsService,public env: EnvService,private tokenStorage: TokenStorageService) { }
+
+  getFileItems(): Observable<any[]> {
+    const url = `${this.env.piOpp}getFilesByClassAndObject/27/407`;
+    return this.http.get<any[]>(url);
+  }
+}
