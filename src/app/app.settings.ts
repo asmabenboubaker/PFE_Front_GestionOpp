2a6a3c9fae91685abfe4c19c26a71d03309fe8d2
Asma Ben Boubaker <36500896+asmabenboubaker@users.noreply.github.com>
2024-06-27 09:46:55 +0200

    css Profil

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -8,14 +8,14 @@ export class AppSettings {
         'Angular Admin Template with Bootstrap 4',
         {
             menu: 'vertical', //horizontal , vertical
-            menuType: 'default', //default, compact, mini
+            menuType: 'compact', //default, compact, mini
             showMenu: true,
             navbarIsFixed: true,
             footerIsFixed: false,
             sidebarIsFixed: true,
             showSideChat: false,
             sideChatIsHoverable: true,
-            skin:'combined'  //light , dark, blue, green, combined, purple, orange, brown, grey, pink          
+            skin:'green'  //light , dark, blue, green, combined, purple, orange, brown, grey, pink
         }
     )
 }
